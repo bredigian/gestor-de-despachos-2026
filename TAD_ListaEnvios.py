f18d3082a7e5f5724e29b7c78059c35881636b11
Gianluca Bredice <bredicegian@gmail.com>
2026-05-15 23:07:27 -0300

    feat: add initial methods to ListaEnvios TAD

---
+++ TAD_ListaEnvios.py
@@ -0,0 +1,18 @@
+from TAD_Envio import *
+
+def crearListaEnvios():
+    return []
+
+def agregarEnvio(lista, envio):
+    lista.append(envio)
+
+def buscarEnvioPorID(lista, id):
+    for envio in lista:
+        envioId = verID(envio)
+        if envioId == id:
+            return envio
+    return None
+
+def eliminarEnvio(lista, envio):
+    if envio in lista:
+        lista.remove(envio)
